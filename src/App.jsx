b0c687ddac097ef2f3553c97c352ca714e6fcc8d
Sagar Mainali <sagarmainali78@gmail.com>
2025-11-20 16:31:08 +0545

    feat: added lazy loading on both CMS and Website component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import DashboardLayout from "./pages/user/layout/DashboardLayout";
